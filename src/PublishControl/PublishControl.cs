a62dbc0132130e04dd629888f49e43f3cbe005c1
Justin Karneges <justin@affinix.com>
2013-11-19 00:59:00 -0800

    grip module, add missing files

--- src/PublishControl/PublishControl.cs
+++ src/PublishControl/PublishControl.cs
@@ -17,47 +17,6 @@ namespace RealCrowd.PublishControl
         public abstract object ToObject();
     }
 
-    public class HttpResponseFormat : Format
-    {
-        public int Code { get; set; }
-
-        public string Reason { get; set; }
-
-        public List<KeyValuePair<string, string>> Headers { get; set; }
-
-        public string Body { get; set; }
-
-        public override string Name
-        {
-            get { return "http-response"; }
-        }
-
-        public override object ToObject()
-        {
-            var d = new Dictionary<string, object>();
-            d["code"] = Code;
-            d["reason"] = Reason;
-            d["headers"] = Headers.Select(pair => new List<string>() { pair.Key, pair.Value });
-            d["body"] = Body;
-            return d;
-        }
-    }
-
-    public class FppFormat : Format
-    {
-        public object Value { get; set; }
-
-        public override string Name
-        {
-            get { return "fpp"; }
-        }
-
-        public override object ToObject()
-        {
-            return Value;
-        }
-    }
-
     public class Item
     {
         public List<Format> Formats { get; set; }
--- src/TestApp/Program.cs
+++ src/TestApp/Program.cs
@@ -3,9 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RealCrowd.PublishControl;
 
 namespace TestApp
 {
+    public class FppFormat : Format
+    {
+        public object Value { get; set; }
+
+        public override string Name
+        {
+            get { return "fpp"; }
+        }
+
+        public override object ToObject()
+        {
+            return Value;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
